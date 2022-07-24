38916e10ebc5409e5b77a4182e549b3b00a7c1f0
lst47 <linh20121@gmail.com>
2022-07-24 18:32:32 +0300

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double calculate(int a, int b) {
-    double indexM;
-    indexM = a / ((b*b)*0.0001);
+        double indexM;
+        indexM = a / ((b * b) * 0.0001);
         return indexM;
 
 
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         BmiService service = new BmiService();
         int mass = 70;
         int height = 170;
-        double index = service.calculate(mass,height);
+        double index = service.calculate(mass, height);
         DecimalFormat df = new DecimalFormat("###.##");
         System.out.println(df.format(index));
     }
